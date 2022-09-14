82e8b142fa392078a4fae3cc48496e2759c62963
Cjenae7 <104477982+Cjenae7@users.noreply.github.com>
2022-09-14 18:49:14 -0500

    Added the search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./sand volleyball.jpg";
-import "./App.css";
+import "./Dictionary";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid " alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid " alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>Coded by C.Sneed</small>
+        </footer>
+      </div>
     </div>
   );
 }
